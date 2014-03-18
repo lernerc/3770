36e2e2b404b68403bf05aa798c11622a33c33b72
Camara Lerner <camara.lerner@gmail.com>
2014-03-18 13:27:51 -0600

    got the rectangles drawn and have them highlighting and moving

--- as3/testdrag.cpp
+++ as3/testdrag.cpp
@@ -1,13 +1,59 @@
 #include "testdrag.h"
- 
-TestDrag::TestDrag(int w, int h, int iw, int tw, QWidget *p) : QWidget(p) {
+#include <QPaintEvent>
+#include <QPainter>
+#include <QRect>
+#include <QMouseEvent>
 
+TestDrag::TestDrag(int w, int h, int iw, int tw, QWidget *p) : QWidget(p) {
+   background = QRect(0, 0, w, h);
+   icon = QRect( 10, 10, iw, iw);
+   target = QRect(w-tw-10, h-tw-10, tw, tw);
+   QWidget::setMouseTracking(true);
+   setMinimumSize(w+2, h+2);
+   icon_pen = QPen(Qt::darkGreen);
+   icon_pen.setWidth(1);
+   target_pen = QPen(Qt::darkBlue);
+   target_pen.setWidth(1);
+   update();
 }
 
 void TestDrag::reset() {
 
 }
 
-void TestDrag::testComplete(int, double, int) {
+void TestDrag::mousePressEvent(QMouseEvent* event) {
+   pos = event->pos();
+   if(icon.contains(event->pos()))
+      icon_pen.setWidth(3);
+   update();
+}
 
+void TestDrag::mouseReleaseEvent(QMouseEvent* event) {
+   icon_pen.setWidth(1);
+   target_pen.setWidth(1);
+   update();
+}
+
+void TestDrag::mouseMoveEvent(QMouseEvent* event) {
+   if(icon_pen.width() == 3) {
+      icon.translate(event->pos() - pos);
+      pos=event->pos();
+   }
+   
+   
+   if(target.contains(icon, true) && icon_pen.width() == 3)
+      target_pen.setWidth(3);
+   else
+      target_pen.setWidth(1);
+   update();
+}
+
+
+void TestDrag::paintEvent(QPaintEvent *event) {
+   QPainter p(this);
+   p.drawRect(background);
+   p.setPen(target_pen);
+   p.drawRect(target);
+   p.setPen(icon_pen);
+   p.drawRect(icon);
 }
--- as3/testdrag.h
+++ as3/testdrag.h
@@ -1,16 +1,30 @@
 #ifndef TESTDRAG_H
 #define TESTDRAG_H
 #include <QWidget>
+#include <QPen>
+#include <QPoint>
+
+class QPaintEvent;
+class QRect;
+class QMouseEvent;
 
 class TestDrag : public QWidget {
   Q_OBJECT
  public:
-  TestDrag(int w, int h, int iw, int tw, QWidget *p);
+  TestDrag(int w=600, int h=400, int iw=25, int tw=100, QWidget *p=0);
  public slots:
   void reset();
  signals:
   void testComplete(int, double, int);
-
+  protected:
+  virtual void paintEvent(QPaintEvent *event);
+  virtual void mousePressEvent(QMouseEvent* event);
+  virtual void mouseReleaseEvent(QMouseEvent* event);
+  virtual void mouseMoveEvent(QMouseEvent* event);
+  private:
+  QRect background, icon, target;
+  QPen icon_pen, target_pen;
+  QPoint pos;
 };
 
 #endif
